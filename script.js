8ba526d1a8cec71f596628237e5025c985272f30
Cody Malay <45500765+codymalay@users.noreply.github.com>
2023-02-09 08:56:09 -0800

    add comments to functions to be built at a later date

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ function getComputerChoice() {
     } else (choice <= 1 ? choice = "Paper" : choice = "Scissors")
     return choice;
 }
+// a function call that compares player choice vs cpu choice and outputs a string based on the winner of the game
+function playRound(playerChoice, computerChoice) {
+    // use a for loop to condition results for each player choice? Switch case? TBD
+}
+// a game will consist of 5 rounds, winner is the first to win 3 rounds
+function game() {
+    // 
+}
